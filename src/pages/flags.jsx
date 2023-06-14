e9847bddd0912fc214540395e5e7f90bb3d1c86f
gmarvis <sgmarvis@gmail.com>
2023-06-14 05:20:43 +0100

    flags displayed on the dom

--- src/pages/flags.jsx
+++ src/pages/flags.jsx
@@ -1,32 +1,12 @@
 /* eslint-disable */
 import React, { useEffect, useState } from "react";
-// import { json } from "react-router-dom";
-// import Data from "../../public/data/football.json";
-// import { useEffect } from "react";
-// import { json } from "react-router-dom";
 
 export const Flags = () => {
-  // const [flagData, setFlagData] = useState();
-
-  // const getData = (url) => fetch(url);
-
-  // const getData = () => {
-  //   fetch(football.json, {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Accept: "application/json",
-  //     },
-  //   })
-
-  //   .then(function(response){
-  //     return response.json();
-  //   })
-  //   .then(function(data){
-  //     console.log(data)
-  //   })
+  const [clubs, setClubs] = useState([]);
+  const [countries, setCountries] = useState([]);
 
   const getData = () => {
-    fetch('football.json', {
+    fetch("football.json", {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
@@ -35,10 +15,17 @@ export const Flags = () => {
       .then(function (response) {
         return response.json();
       })
-      .then(function (myJson) {
-        console.log('my teams',myJson);
+      .then(function (data) {
+        setClubs(data.clubs);
+        setCountries(data.countries);
+        // console.log("team =", data);
+        return data;
       });
   };
+  console.clear;
+  console.log("clubs", clubs);
+  console.log("countries, ", countries);
+
   useEffect(() => {
     getData();
   }, []);
@@ -53,12 +40,32 @@ export const Flags = () => {
           placeholder="search team"
         />
         <div className="Cf">
-          <div>
+          <div className="teams">
             <h3>Clountries</h3>
+            <div className="countries">
+              {countries?.map((team) => {
+                return (
+                  <div key={team.country} className="teamSection">
+                    <img className="teamFlags" src={team.flag} alt="flag" />
+                    <p>{team.country}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
 
-          <div>
+          <div className="teams">
             <h3>Clubs</h3>
+            <div className="countries">
+              {clubs?.map((club) => {
+                return (
+                  <div key={club.name} className="teamSection">
+                    <img className="teamFlags" src={club.url} alt="flag" />
+                    <p>{club.name}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
