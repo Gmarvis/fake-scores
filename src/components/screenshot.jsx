dcef8f50f525841f95243a1e5a59dac88b003486
gmarvis <sgmarvis@gmail.com>
2023-06-16 16:55:20 +0100

    added screen capture funtionality

--- src/components/screenshot.jsx
+++ src/components/screenshot.jsx
@@ -1,31 +1,31 @@
-import React, { createRef } from "react";
-import { useScreenshot, createFileName } from "use-react-screenshot";
+// import React, { createRef } from "react";
+// import { useScreenshot, createFileName } from "use-react-screenshot";
 
-export const ScreenShot = () => {
-  const ref = createRef(null);
+// export const ScreenShot = () => {
+//   const ref = createRef(null);
 
-  const [image, takeScreenshot] = useScreenshot({
-    type: "image/jpeg",
-    quality: 1.0,
-  });
+//   const [image, takeScreenshot] = useScreenshot({
+//     type: "image/jpeg",
+//     quality: 1.0,
+//   });
 
-  const download = (
-    image,
-    { name = "screen capture", extension = "jpg" } = {}
-  ) => {
-    const a = document.createElement("a");
-    a.href = image;
-    a.download = createFileName(extension, name);
-    a.click();
-  };
+//   const download = (
+//     image,
+//     { name = "screen capture", extension = "jpg" } = {}
+//   ) => {
+//     const a = document.createElement("a");
+//     a.href = image;
+//     a.download = createFileName(extension, name);
+//     a.click();
+//   };
 
-  const downloadScreenshot = () => {
-    takeScreenshot(ref.current).then(download);
-  };
+//   const downloadScreenshot = () => {
+//     takeScreenshot(ref.current).then(download);
+//   };
 
-  return (
-    <div>
-      <button onClick={downloadScreenshot} className="bg-white text-blue-900">capture</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <button onClick={downloadScreenshot} className="bg-white text-blue-900">capture</button>
+//     </div>
+//   );
+// };
